b126f883d6a090eee9ce0367f640dd435e83cd66
hyojoonm <ssjj008z@naver.com>
2022-08-22 16:05:08 +0900

    commit

---
+++ src/main/java/com/codestates/section3week1/MemberPostDto.java
@@ -0,0 +1,43 @@
+package com.codestates.section3week1;
+
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+public class MemberPostDto {
+     @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank(message = "이름은 공백이 아니어야 됨 ")
+    private String name;
+
+    @Pattern(regexp = "^010-\\d{3,4}-\\d{4}$" ,
+            message = "휴대폰 번호는 010으로 시작하는 11자리 숫자와 '-'로 구성되외어야 함")
+    private String phone;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
--- src/main/java/com/codestates/section3week1/member/MemberController.java
+++ src/main/java/com/codestates/section3week1/member/MemberController.java
@@ -2,41 +2,61 @@ package com.codestates.section3week1.member;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-
+import com.codestates.section3week1.MemberPatchDto;
+import com.codestates.section3week1.MemberPostDto;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
 import java.util.HashMap;
 import java.util.Map;
 
-@Slf4j
 @RestController
 @RequestMapping("/v1/members")
+@Validated
 public class MemberController {
 
-    @PostMapping
-    public ResponseEntity postMember(@RequestParam("email") String email,
-                             @RequestParam("name") String name,
-                             @RequestParam("phone") String phone) {
-       Map<String,String> map = new HashMap<>();
-       map.put("email",email);
-       map.put("name",name);
-       map.put("phone", phone);
 
-       return new ResponseEntity<>(map,HttpStatus.CREATED);
+
+    // 회원 정보 수정
+
+    @PatchMapping("/{member-id}")
+    public ResponseEntity patchMember(@PathVariable("member-id") @Min(1) long memberId,
+                                      @Valid @RequestBody MemberPatchDto memberPatchDto) {
+        memberPatchDto.setMemberId(memberId);
+
+        // No need Business logic
+
+        return new ResponseEntity<>(memberPatchDto, HttpStatus.OK);
     }
 
+    // 한명의 회원 정보 조회
     @GetMapping("/{member-id}")
-    public ResponseEntity getMember(@PathVariable("member-id")long memberId){
+    public ResponseEntity getMember(@PathVariable("member-id") long memberId) {
         System.out.println("# memberId: " + memberId);
 
+        // not implementation
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    // 모든 회원 정보 조회
     @GetMapping
-    public ResponseEntity getMembers(){
+    public ResponseEntity getMembers() {
         System.out.println("# get Members");
 
+        // not implementation
+
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    // 회원 정보 삭제
+    @DeleteMapping("/{member-id}")
+    public ResponseEntity deleteMember(@PathVariable("member-id") long memberId) {
+        // No need business logic
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
